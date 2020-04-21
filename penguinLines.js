99548b7d3d2f76ebbbd4142963727cae2357a477
eanelson99 <60753894+eanelson99@users.noreply.github.com>
2020-04-20 21:49:39 -0400

    Update penguinLines.js

--- penguinLines.js
+++ penguinLines.js
@@ -8,14 +8,40 @@ var getPenguinQuiz = function(penguin)
         return QuizGrades
     }
 
+var drawToolTip = function(penguin)
+{
+    d3.select("#tooltip div")
+        .remove();
+
+    
+    var xPosition = d3.event.pageX;
+    var yPosition = d3.event.pageY;
+
+    var base = d3.select("#tooltip")
+        .classed("hidden",false)
+        .style("top",yPosition+"px")
+        .style("left",xPosition+"px")
+        .append("div")
+    
+    base.append("div")
+        .classed("tt-Title",true)
+        .text("Penguin Line Selected:");
+    
+    base.append('img')
+        .attr("id","photo")
+        .attr('src','imgs/'+penguin.picture);
+    
+}
+
 
 var drawLines = function(penguins,target,graph,yScale,xScale)
 {
     var lineGenerator = d3.line()
-    .x(function(penguin,index) 
+    .x(function(quiz,index) 
        {return xScale(index)})
-    .y(function(penguin)  
-       {return yScale(getQuizScores)})
+    .y(function(quiz)  
+       {return yScale(quiz.grade)})
+    .curve(d3.curveCardinal)
     
     var lines = d3.select(target)
         .select(".graph")
@@ -27,13 +53,61 @@ var drawLines = function(penguins,target,graph,yScale,xScale)
         .attr("fill","none")
         .attr("stroke","gray")
         .attr("stroke-width", 1)
-    
+        .on("mouseover",function(penguin)
+           {
+            d3.select(this)
+            .attr("stroke","black")
+            .attr("stroke-width",7)
+            .raise(this);
+            drawToolTip(penguin)
+           })
+        .on("mouseout",function(penguin)
+           {
+            d3.select(this)
+            .attr("stroke","gray")
+            .attr("stroke-width", 1);
+            d3.select("#tooltip")
+            .classed("hidden",true);
+           })
+        
     lines.append("path")
         .datum(function(penguin)
-        {return getPenguinQuiz})
+        {return penguin.quizes})
         .attr("d", lineGenerator)
     
 }
+
+var createLabels = function(screen,margins,graph,target)
+{
+        var labels = d3.select(target)
+        .append("g")
+        .classed("labels",true)
+        
+    labels.append("text")
+        .text("Grade Over Time")
+        .classed("title",true)
+        .attr("text-anchor","middle")
+        .attr("x",margins.left+(graph.width/2))
+        .attr("y",margins.top)
+    
+    labels.append("text")
+        .text("Week")
+        .classed("label",true)
+        .attr("text-anchor","middle")
+        .attr("x",margins.left+(graph.width/2))
+        .attr("y",screen.height)
+    
+    labels.append("g")
+        .attr("transform","translate(20,"+ 
+              (margins.top+(graph.height/2))+")")
+        .append("text")
+        .text("Change in Quiz Grade")
+        .classed("label",true)
+        .attr("text-anchor","middle")
+        .attr("transform","rotate(90)")
+    
+}
+
                
 var createAxes = function(screen,margins,target,graph,xScale,yScale)
 {
@@ -55,9 +129,9 @@ var createAxes = function(screen,margins,target,graph,xScale,yScale)
 
 var displayLineGraph = function(target, penguins)
 {  
-    var screen = {width:800, height:600};
+    var screen = {width:1000, height:600};
 
-    var margins = {top:15, bottom:40, left:70, right:40};
+    var margins = {top:50, bottom:40, left:70, right:40};
     
     var graph = 
         {
@@ -91,9 +165,10 @@ var displayLineGraph = function(target, penguins)
    var yScale = d3.scaleLinear()
         .domain([lowQuiz,highQuiz])
         .range([graph.height,0])
-    
+             
   createAxes(screen,margins,target,graph,xScale,yScale)
   drawLines(penguins,target,graph,yScale,xScale)
+  createLabels(screen,margins,graph,target)
  
     
 }
